2c8dbe3eeabea9ee96c3f7f07385be59ea990b75
Quillinan <milton.quillinan@gmail.com>
2023-07-31 11:48:37 -0300

    get customer by id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -8,6 +8,25 @@ export const customersController = {
       res.status(500).send(err.message);
     }
   },
+  getCustomerById: async (req, res) => {
+    const customerId = req.params.id;
+
+    try {
+      const query = "SELECT * FROM customers WHERE id = $1;";
+      const result = await connection.query(query, [customerId]);
+
+      if (result.rows.length === 0) {
+        return res.status(404).json({ error: "Cliente não encontrado." });
+      }
+
+      const customer = result.rows[0];
+      res.status(200).json(customer);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ error: "Erro ao buscar cliente.", details: err.message });
+    }
+  },
   // postGame: async (req, res) => {
   //   try {
   //     const { name } = req.body;
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -3,7 +3,10 @@ import { customersController } from "../controllers/customers.controller.js";
 
 const customersRouter = Router();
 
-// Rota para obter todos os games
+// Rota para obter todos os clientes
 customersRouter.get("/", customersController.getCustomers);
+
+// Rota para obter cliente pelo id
+customersRouter.get("/:id", customersController.getCustomerById);
 
 export default customersRouter;
